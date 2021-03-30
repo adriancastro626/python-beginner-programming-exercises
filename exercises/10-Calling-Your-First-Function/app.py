59df6b945194a4c11ba480e3007b6862af61f708
adriancastro626 <adriancastro626@icloud.com>
2021-03-30 02:52:22 +0000

    first

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -2,5 +2,9 @@ def is_odd(my_number):
   	return (my_number % 2 != 0)
 
 
-def my_main_code():
-    # your code here
+def my_main_code():     
+    x = is_odd(45345)
+    print(x)
+    
+my_main_code()    
+  
