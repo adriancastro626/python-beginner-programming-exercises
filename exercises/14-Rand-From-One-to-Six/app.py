aac1399ee28f4e3664a41738bb4d19b2f43ee8ce
adriancastro626 <adriancastro626@icloud.com>
2021-03-31 01:48:14 +0000

    update

--- exercises/14-Rand-From-One-to-Six/app.py
+++ exercises/14-Rand-From-One-to-Six/app.py
@@ -1,9 +1,11 @@
 import random
 
 def get_randomInt():
-	random_number = random.randrange(0,12)
-	return random_number
+    random_number = random.randrange(1,13)
+    print(random_number)    
+    return random_number
+        
+        
+get_randomInt()
 
 
-
-print(get_randomInt())
